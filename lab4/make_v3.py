7b806048c1ea5f2427b10e2a7b43804a0ef280fb
MateLr <utenkov2003@mail.ru>
2026-04-27 14:21:21 +0500

    data v3: one-hot encode Sex

---
+++ lab4/make_v3.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv("data/titanic.csv")
+
+df = pd.get_dummies(df, columns=["Sex"], prefix="Sex").astype(
+    {"Sex_female": int, "Sex_male": int}
+)
+
+df.to_csv("data/titanic.csv", index=False)
+print(f"{list(df.columns)}")
